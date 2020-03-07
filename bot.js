95979b58e753f6cce112d478805d84d68634b6ed
momen7 <45073137+momen7@users.noreply.github.com>
2020-03-07 23:12:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
+client.user.setGame(`Fuck My Life`,"http://twitch.tv/REBEL.-X")
 client.user.setStatus("dnd")
 });
 
